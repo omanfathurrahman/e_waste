0bcdec2e40d0c1539e306c8bb28585d2ab65d784
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-22 12:07:20 +0700

    refactor: remove MaterialApp widget, refactor others

--- lib/screen/service/detail/detail_lokasi_service.dart
+++ lib/screen/service/detail/detail_lokasi_service.dart
@@ -18,88 +18,85 @@ class DetailLokasiServiceScreen extends StatefulWidget {
 class _DetailLokasiServiceScreenState extends State<DetailLokasiServiceScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.00, 1.00),
-                end: Alignment(0, -1),
-                colors: [Color(0xFFE9EBFF), Color(0xFF8B97FF)],
-              ),
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          clipBehavior: Clip.antiAlias,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.00, 1.00),
+              end: Alignment(0, -1),
+              colors: [Color(0xFFE9EBFF), Color(0xFF8B97FF)],
             ),
           ),
-          Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 20),
-              child: ListView(
-                children: [
-                  KomponenHeader(idKecamatan: widget.idKecamatan),
-                  Container(
-                    color: Colors.white24,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          FutureBuilder(
-                            future: getServiceCenterName(widget.idKecamatan),
-                            builder: (context, snapshot) {
-                              if (!snapshot.hasData) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              }
-                              final data =
-                                  snapshot.data as Map<String, dynamic>;
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    data['nama']!,
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                  Text(
-                                    data['alamat']!,
-                                    style: const TextStyle(
-                                        color: Colors.white, fontSize: 12),
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                          const SizedBox(height: 10),
-                          FutureBuilder(
-                            future: getServiceCenterMaps(
-                                idServiceCenter: widget.idServiceCenter),
-                            builder: (context, snapshot) {
-                              if (!snapshot.hasData) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              }
-                              final imgPath = snapshot.data!;
-                              return ClipRRect(
-                                borderRadius: BorderRadius.circular(8),
-                                child: Image.network(imgPath),
-                              );
-                            },
-                          )
-                        ],
-                      ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 20),
+            child: ListView(
+              children: [
+                KomponenHeader(idKecamatan: widget.idKecamatan),
+                Container(
+                  color: Colors.white24,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        FutureBuilder(
+                          future: getServiceCenterName(widget.idKecamatan),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                            final data = snapshot.data as Map<String, dynamic>;
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data['nama']!,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                Text(
+                                  data['alamat']!,
+                                  style: const TextStyle(
+                                      color: Colors.white, fontSize: 12),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        FutureBuilder(
+                          future: getServiceCenterMaps(
+                              idServiceCenter: widget.idServiceCenter),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                            final imgPath = snapshot.data!;
+                            return ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(imgPath),
+                            );
+                          },
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
@@ -149,8 +146,9 @@ Future<String> getServiceCenterMaps({
   // final response = await supabase.storage
   //     .from('peta_servicecenter')
   //     .download('$idServiceCenter.png');
-  final response = await supabase.storage.from("peta_servicecenter").getPublicUrl(
-      "$idServiceCenter.png");
+  final response = supabase.storage
+      .from("peta_servicecenter")
+      .getPublicUrl("$idServiceCenter.png");
 
   return response;
 }
