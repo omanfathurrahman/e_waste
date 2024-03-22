bcd4faaa186fce851225b04b41c7b682d5c157c0
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-22 12:00:20 +0700

    refactor: remove unused function, remove unused data, refactor others

--- lib/screen/profile/detail_profile/edit_nama/edit_nama.dart
+++ lib/screen/profile/detail_profile/edit_nama/edit_nama.dart
@@ -1,7 +1,6 @@
 import 'package:ewaste/main.dart';
 import 'package:ewaste/screen/profile/detail_profile/detail_profile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class EditNamaScreen extends StatefulWidget {
   const EditNamaScreen({
@@ -30,13 +29,6 @@ class _EditNamaScreenState extends State<EditNamaScreen> {
         .then((value) => _namaController.text = value['nama_lengkap']);
 
     super.initState();
-  }
-
-  Future<Map<String, dynamic>> _getUser(id) async {
-    final response =
-        await supabase.from('profile').select().eq('id', id).single().limit(1);
-
-    return response;
   }
 
   Future<void> _updateNama() async {
@@ -70,112 +62,98 @@ class _EditNamaScreenState extends State<EditNamaScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _getUser(userId),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final data = snapshot.data as Map<String, dynamic>;
-          return MaterialApp(
-            home: Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment(0.00, 1.00),
-                      end: Alignment(0, -1),
-                      colors: [Color(0xFFE9EBFF), Color(0xFF8B97FF)],
-                    ),
-                  ),
-                ),
-                Scaffold(
-                  backgroundColor: Colors.transparent,
-                  body: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
-                    child: ListView(
-                      children: [
-                        const KomponenHeader(),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white54,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 12),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "Update Nama Lengkap",
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    TextField(
-                                      controller: _namaController,
-                                      decoration: InputDecoration(
-                                        labelText: "Nama Lengkap",
-                                        hintText: "Masukkan Nama Lengkap Anda",
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        _isLoading
-                                            ? const Center(
-                                                child:
-                                                    CircularProgressIndicator(),
-                                              )
-                                            : ElevatedButton(
-                                                onPressed: () {
-                                                  _updateNama();
-                                                },
-                                                child: const Text("Simpan"),
-                                              ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+    return  Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.00, 1.00),
+                end: Alignment(0, -1),
+                colors: [Color(0xFFE9EBFF), Color(0xFF8B97FF)],
+              ),
             ),
-          );
-        });
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              child: ListView(
+                children: [
+                  const KomponenHeader(),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Update Nama Lengkap",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextField(
+                                controller: _namaController,
+                                decoration: InputDecoration(
+                                  labelText: "Nama Lengkap",
+                                  hintText: "Masukkan Nama Lengkap Anda",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  _isLoading
+                                      ? const Center(
+                                          child: CircularProgressIndicator(),
+                                        )
+                                      : ElevatedButton(
+                                          onPressed: () {
+                                            _updateNama();
+                                          },
+                                          child: const Text("Simpan"),
+                                        ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      
+    );
   }
 }
 
