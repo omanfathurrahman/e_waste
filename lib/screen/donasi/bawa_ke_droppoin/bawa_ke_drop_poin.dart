d12488dda01d6942b1bfb8ea9b34f604b43ac295
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-22 11:42:30 +0700

    refactor, fix: refactor new lines, remove button component, fix address component

--- lib/screen/donasi/bawa_ke_droppoin/bawa_ke_drop_poin.dart
+++ lib/screen/donasi/bawa_ke_droppoin/bawa_ke_drop_poin.dart
@@ -1,13 +1,17 @@
+import 'package:ewaste/main.dart';
+import 'package:ewaste/screen/donasi/daftar_droppoin/daftar_droppoin.dart';
 import 'package:flutter/material.dart';
 
 class BawaKeDropPointDonasiScreen extends StatefulWidget {
   const BawaKeDropPointDonasiScreen({super.key});
 
   @override
-  State<BawaKeDropPointDonasiScreen> createState() => _BawaKeDropPointDonasiScreenState();
+  State<BawaKeDropPointDonasiScreen> createState() =>
+      _BawaKeDropPointDonasiScreenState();
 }
 
-class _BawaKeDropPointDonasiScreenState extends State<BawaKeDropPointDonasiScreen> {
+class _BawaKeDropPointDonasiScreenState
+    extends State<BawaKeDropPointDonasiScreen> {
   final TextEditingController alamat = TextEditingController();
 
   @override
@@ -35,7 +39,6 @@ class _BawaKeDropPointDonasiScreenState extends State<BawaKeDropPointDonasiScree
               const SizedBox(height: 16),
               KomponenAlamat(alamat: alamat),
               const SizedBox(height: 16),
-              const KomponenTombol(),
             ],
           ),
         ),
@@ -55,7 +58,7 @@ class KomponenHeader extends StatelessWidget {
           Navigator.of(context).pop();
         }),
         const Text(
-          "Buang Sampah Elektronik",
+          "Donasi Sampah Elektronik",
           style: TextStyle(
               fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
         ),
@@ -63,10 +66,132 @@ class KomponenHeader extends StatelessWidget {
     );
   }
 }
-
-class KomponenAlamat extends StatelessWidget {
+class KomponenAlamat extends StatefulWidget {
   const KomponenAlamat({super.key, required this.alamat});
   final TextEditingController alamat;
+
+  @override
+  State<KomponenAlamat> createState() => _KomponenAlamatState();
+}
+enum PilihanAlamat { alamatSaatIni, aturUlang }
+class _KomponenAlamatState extends State<KomponenAlamat> {
+  PilihanAlamat? _pilihanAlamat = PilihanAlamat.alamatSaatIni;
+  late String userId;
+  // bool _isLoading = false;
+  final TextEditingController _kabupatenKotaController =
+      TextEditingController();
+  final TextEditingController _kecamatanController = TextEditingController();
+  final TextEditingController _kelurahanDesaController =
+      TextEditingController();
+
+  List<dynamic> curListKecamatan = [];
+  List<dynamic> curListKelurahanDesa = [];
+
+  @override
+  void initState() {
+    userId = supabase.auth.currentUser!.id;
+    // _inisializeData();
+    _getAlamatOption();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _kabupatenKotaController.dispose();
+    _kecamatanController.dispose();
+    _kelurahanDesaController.dispose();
+    super.dispose();
+  }
+
+  Future<Map<String, dynamic>> _getUserAlamat(id) async {
+    final alamat = await supabase
+        .from('profile')
+        .select('alamat_id, detail_alamat')
+        .eq('id', id)
+        .single()
+        .limit(1);
+    final detailAlamat = await supabase
+        .from('daftar_alamat')
+        .select()
+        .eq('id', alamat['alamat_id'])
+        .single()
+        .limit(1);
+    return detailAlamat;
+  }
+
+  Future<List<Map<String, dynamic>>> _getAlamatOption() async {
+    final alamat = await supabase.from('daftar_alamat').select();
+    return alamat;
+  }
+
+  // Future<void> _updateSelectedAlamat() async {
+  //   setState(() {
+  //     // _isLoading = true;
+  //   });
+  //   var selectedAlamatId = await supabase
+  //       .from('daftar_alamat')
+  //       .select('id')
+  //       .eq('kabupaten_kota', _kabupatenKotaController.text)
+  //       .eq('kecamatan', _kecamatanController.text)
+  //       .eq('kelurahan_desa', _kelurahanDesaController.text)
+  //       .single()
+  //       .limit(1);
+
+  //   await supabase.from('profile').update({
+  //     'alamat_id': selectedAlamatId['id'],
+  //   }).eq('id', userId);
+  //   setState(() {
+  //     // _isLoading = false;
+  //   });
+  // }
+
+  Future<void> _getAllKecamatan() async {
+    final kecamatan = await supabase
+        .from('daftar_alamat')
+        .select('kecamatan')
+        .eq('kabupaten_kota', _kabupatenKotaController.text);
+    setState(() {
+      curListKecamatan =
+          kecamatan.map((item) => item['kecamatan']).toSet().toList();
+    });
+  }
+
+  Future<void> _getAllKelurahanDesa() async {
+    final kelurahanDesa = await supabase
+        .from('daftar_alamat')
+        .select('kelurahan_desa')
+        .eq('kecamatan', _kecamatanController.text);
+    setState(() {
+      curListKelurahanDesa =
+          kelurahanDesa.map((item) => item['kelurahan_desa']).toSet().toList();
+    });
+  }
+
+  Future<void> _onSelectedCurAlamat() async {
+    final alamat = await _getUserAlamat(userId);
+    _kabupatenKotaController.text = alamat['kabupaten_kota'];
+    _kecamatanController.text = alamat['kecamatan'];
+    _kelurahanDesaController.text = alamat['kelurahan_desa'];
+  }
+
+  Future<void> _onSelectedAturUlangAlamat() async {
+    _kabupatenKotaController.clear();
+    _kecamatanController.clear();
+    _kelurahanDesaController.clear();
+  }
+
+  Future<num> _getSelectedAlamatId() async {
+    final selectedAlamatId = await supabase
+        .from('daftar_alamat')
+        .select('id, kabupaten_kota, kecamatan, kelurahan_desa')
+        .eq('kabupaten_kota', _kabupatenKotaController.text)
+        .eq('kecamatan', _kecamatanController.text)
+        .eq('kelurahan_desa', _kelurahanDesaController.text)
+        .single()
+        .limit(1);
+
+    return selectedAlamatId['id'] as num;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,31 +205,130 @@ class KomponenAlamat extends StatelessWidget {
         const SizedBox(
           height: 16,
         ),
-        TextField(
-          controller: alamat,
-          decoration: const InputDecoration(
-            hintStyle: TextStyle(color: Colors.white),
-            hintText: "Masukkan alamat",
-            focusedBorder: OutlineInputBorder(
-              borderSide:
-                  BorderSide(width: 1, color: Colors.white), //<-- SEE HERE
+        Column(
+          children: [
+            RadioListTile(
+              value: PilihanAlamat.alamatSaatIni,
+              groupValue: _pilihanAlamat,
+              title: const Text(
+                "Alamat Saat Ini",
+                style: TextStyle(color: Colors.white),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _pilihanAlamat = value as PilihanAlamat;
+                  _onSelectedCurAlamat();
+                });
+              },
             ),
-            enabledBorder: OutlineInputBorder(
-              borderSide:
-                  BorderSide(width: 1, color: Colors.white), //<-- SEE HERE
+            RadioListTile(
+              value: PilihanAlamat.aturUlang,
+              groupValue: _pilihanAlamat,
+              title: const Text(
+                "Atur Ulang Alamat",
+                style: TextStyle(color: Colors.white),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _onSelectedAturUlangAlamat();
+                  _pilihanAlamat = value as PilihanAlamat;
+                });
+              },
             ),
-          ),
-        )
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        _pilihanAlamat == PilihanAlamat.aturUlang
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Kabupaten/Kota",
+                      style: TextStyle(color: Colors.black)),
+                  FutureBuilder(
+                    future: _getAllKabupatenKota(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final kabupatenKota = snapshot.data!;
+                      return DropdownMenu(
+                          onSelected: (value) {
+                            _getAllKecamatan();
+                          },
+                          controller: _kabupatenKotaController,
+                          expandedInsets: EdgeInsets.zero,
+                          dropdownMenuEntries: kabupatenKota
+                              .map((e) => DropdownMenuEntry(value: e, label: e))
+                              .toList());
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text("Kecamatan",
+                      style: TextStyle(color: Colors.black)),
+                  DropdownMenu(
+                    onSelected: (value) {
+                      _getAllKelurahanDesa();
+                    },
+                    enableFilter: true,
+                    controller: _kecamatanController,
+                    expandedInsets: EdgeInsets.zero,
+                    dropdownMenuEntries: curListKecamatan
+                        .map((e) => DropdownMenuEntry(value: e, label: e))
+                        .toList(),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text("Kelurahan/Desa",
+                      style: TextStyle(color: Colors.black)),
+                  DropdownMenu(
+                    onSelected: (value) {
+                      _kelurahanDesaController.text = value;
+                    },
+                    controller: _kelurahanDesaController,
+                    expandedInsets: EdgeInsets.zero,
+                    dropdownMenuEntries: curListKelurahanDesa
+                        .map((e) => DropdownMenuEntry(value: e, label: e))
+                        .toList(),
+                  ),
+                ],
+              )
+            : Container(),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton(
+            onPressed: () => {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => FutureBuilder(
+                          future: _getSelectedAlamatId(),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            final alamatId = snapshot.data!;
+                            return DaftarDroppoinDonasi(alamatId: alamatId);
+                          }),
+                    ),
+                  ),
+                },
+            child: const Text("Cari"))
       ],
     );
   }
 }
 
-class KomponenTombol extends StatelessWidget {
-  const KomponenTombol({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () => {}, child: const Text("Cari"));
-  }
+Future<List<dynamic>> _getAllKabupatenKota() async {
+  final kabupatenKota =
+      await supabase.from('daftar_alamat').select('kabupaten_kota');
+  return kabupatenKota.map((item) => item['kabupaten_kota']).toSet().toList();
 }
