f736034dc9bc3a28522e6f38fab7bf4362d2339c
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-03-22 11:50:24 +0700

    feat, style: add logo, make data dinamic, refactor others

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,10 @@
 import 'package:ewaste/component/icon_widget.dart';
+import 'package:ewaste/main.dart';
+import 'package:ewaste/screen/profile/riwayat_buang_donasi/riwayat_buang_donasi.dart';
+import 'package:ewaste/screen/tukar_poin/tukar_poin.dart';
+import 'package:ewaste/utils/format_date.dart';
+import 'package:ewaste/utils/get_user.dart';
+import 'package:ewaste/utils/get_username.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,20 +16,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late String userId;
+  @override
+  void initState() {
+    userId = supabase.auth.currentUser!.id;
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Stack(
       children: [
-        const KomponenHeader(),
-        const SizedBox(height: 20),
-        const KomponenPoin(),
-        const SizedBox(height: 20),
-        KomponenNavigasi(gantiScreen: widget.gantiScreen),
-        const SizedBox(height: 20),
-        const KomponenIklan(),
-        const SizedBox(height: 20),
-        const Komponen5(),
+        ListView(
+          children: [
+            const KomponenHeader(),
+            const SizedBox(height: 20),
+            KomponenPoin(userId: userId),
+            const SizedBox(height: 20),
+            KomponenNavigasi(gantiScreen: widget.gantiScreen),
+            const SizedBox(height: 20),
+            // const KomponenIklan(),
+            const SizedBox(height: 20),
+            const Komponen5(),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: Image.asset("assets/images/logo-b.png", width: 60, height: 60),
+        ),
       ],
     );
   }
@@ -39,34 +61,61 @@ class KomponenHeader extends StatelessWidget {
       children: [
         Row(
           children: [
-            Image.asset(
-              "assets/images/143035475_1029718927553677_5869815052519506947_n2.png",
+            FutureBuilder(
+              future: getImgUrl(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                final data = snapshot.data;
+                return CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage((data is String)
+                      ? data
+                      : "https://oexltokstwraweaozqav.supabase.co/storage/v1/object/public/avatars/default.webp?t=2024-02-09T13%3A54%3A15.630Z"),
+                );
+              },
             ),
             const SizedBox(width: 10),
-            const Text(
-              "John Mayer",
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            )
+            FutureBuilder(
+              future: getUsername(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                final data = snapshot.data as String;
+                return Text(
+                  data,
+                  style: const TextStyle(fontSize: 20, color: Colors.white),
+                );
+              },
+            ),
           ],
         ),
-        ClipOval(
-          child: Material(
-            color: Colors.white, // Button color
-            child: InkWell(
-              splashColor: Colors.deepPurple[400], // Splash color
-              onTap: () {},
-              child: const SizedBox(
-                  width: 40, height: 40, child: IconWidget("notification")),
-            ),
-          ),
-        )
+        // ClipOval(
+        //   child: Material(
+        //     color: Colors.white, // Button color
+        //     child: InkWell(
+        //       splashColor: Colors.deepPurple[400], // Splash color
+        //       onTap: () {},
+        //       child: const SizedBox(
+        //           width: 40,
+        //           height: 40,
+        //           child: Padding(
+        //             padding: EdgeInsets.all(8.0),
+        //             child: IconWidget("notification"),
+        //           )),
+        //     ),
+        //   ),
+        // )
       ],
     );
   }
 }
 
 class KomponenPoin extends StatelessWidget {
-  const KomponenPoin({super.key});
+  const KomponenPoin({super.key, required this.userId});
+  final String userId;
 
   @override
   Widget build(BuildContext context) {
@@ -80,32 +129,53 @@ class KomponenPoin extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Column(
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   "Jumlah Poin",
                   style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.w300,
                       color: Colors.grey),
                 ),
-                SizedBox(height: 6),
+                const SizedBox(height: 6),
                 Row(
                   children: [
-                    IconWidget(
+                    const IconWidget(
                       "star",
                       panjang: 24,
                       color: Color.fromARGB(255, 62, 135, 230),
                     ),
-                    SizedBox(width: 10),
-                    Text("1024"),
+                    const SizedBox(width: 10),
+                    FutureBuilder(
+                      future: getUser(userId),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const CircularProgressIndicator();
+                        }
+                        final data = snapshot.data as Map<String, dynamic>;
+                        return Text(
+                          data['jumlah_poin'].toString(),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        );
+                      },
+                    ),
                   ],
                 )
               ],
             ),
             ElevatedButton(
-                onPressed: () => {},
+                onPressed: () => {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const TukarPoinScreen(),
+                        ),
+                      )
+                    },
                 child: const Text(
                   "Tukar",
                 ))
@@ -239,8 +309,40 @@ class KomponenIklan extends StatelessWidget {
   }
 }
 
-class Komponen5 extends StatelessWidget {
+class Komponen5 extends StatefulWidget {
   const Komponen5({super.key});
+
+  @override
+  State<Komponen5> createState() => _Komponen5State();
+}
+
+class _Komponen5State extends State<Komponen5> {
+  @override
+  initState() {
+    super.initState();
+    _getAllRiwayat();
+  }
+
+  Future<List<Map<String, dynamic>>> _getAllRiwayat() async {
+    final dataBuang = await supabase
+        .from("sampah_dibuang")
+        .select()
+        .eq("id_user", supabase.auth.currentUser!.id);
+
+    final dataDonasi = await supabase
+        .from("sampah_didonasikan")
+        .select()
+        .eq("id_user", supabase.auth.currentUser!.id);
+    dataDonasi.addAll(dataBuang);
+    final semuaData = dataDonasi;
+    semuaData.sort((a, b) => b["created_at"].compareTo(a["created_at"]));
+    if (semuaData.isEmpty) {
+      return [];
+    }
+    final top5DataTerbaru = semuaData.sublist(0, 5);
+
+    return top5DataTerbaru;
+  }
 
   Widget container({
     required String icon,
@@ -304,7 +406,14 @@ class Komponen5 extends StatelessWidget {
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
             ),
             TextButton(
-              onPressed: () => {},
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RiwayatBuangDonasi(),
+                  ),
+                )
+              },
               child: Text(
                 "Lihat Semua",
                 style: TextStyle(
@@ -314,25 +423,40 @@ class Komponen5 extends StatelessWidget {
             ),
           ],
         ),
-        container(
-          icon: "buang",
-          judul: "Buang Sampah Elektronik",
-          iconColor: const Color(0xFFF7A340),
-          tanggal: "Hari ini, 2 Januari 2021",
-        ),
-        const SizedBox(height: 10),
-        container(
-          icon: "service",
-          judul: "Service Barang Elektronik",
-          iconColor: const Color(0xFF2F80ED),
-          tanggal: "01 Januari 2024",
-        ),
-        const SizedBox(height: 10),
-        container(
-          icon: "buang",
-          judul: "Buang Sampah Elektronik",
-          iconColor: const Color(0xFFF7A340),
-          tanggal: "25 Desember 2023",
+        FutureBuilder(
+          future: _getAllRiwayat(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Text("Riwayat masih kosong");
+            }
+            final semuaData = snapshot.data as List<Map<String, dynamic>>;
+            if (semuaData.isEmpty) {
+              return const Text("Riwayat masih kosong");
+            }
+            return Column(
+              children: semuaData
+                  .map(
+                    (e) => Column(
+                      children: [
+                        const SizedBox(height: 10),
+                        container(
+                          icon: e.containsKey("status_didonasikan")
+                              ? "donasi"
+                              : "buang",
+                          judul: e.containsKey("status_didonasikan")
+                              ? "Donasi Sampah Elektronik"
+                              : "Buang Sampah Elektronik",
+                          tanggal: formatDate(e["created_at"].toString()),
+                          iconColor: e.containsKey("status_didonasikan")
+                              ? const Color(0xFFD11F1F)
+                              : const Color(0xFFF7A340),
+                        ),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            );
+          },
         ),
       ],
     );
